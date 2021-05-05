49389ca44a70678be06710eb578edee39c495cd4
yanafront <yankadubeshko@gmail.com>
2021-05-05 22:25:04 +0300

    add vuex and eslint

---
+++ store/index.js
@@ -0,0 +1,14 @@
+import Vuex from 'vuex';
+
+export const strict = false;
+
+export const state = () => ({
+  article: null,
+  articles: []
+});
+
+export const mutations = {
+  setArticle(state, data) {
+    state.article = data;
+  }
+};
